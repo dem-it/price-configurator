b903d6acc2e84fc2dcde5a3ed03369d5e74ec529
Dennis Rosenbaum <dennis.rosenbaum@outlook.com>
2025-01-26 08:20:09 +0100

    build fix

--- src/pages/auth/configurators/[id]/edit.tsx
+++ src/pages/auth/configurators/[id]/edit.tsx
@@ -76,7 +76,7 @@ const ConfiguratorEditPage = () => {
   const getTemplate = (content: JSX.Element, outerContent: JSX.Element | undefined = undefined) => {
     const host = typeof window !== "undefined" ? window.location.host : ""
     const http = host.includes("localhost") ? "http" : "https"
-    const previewUrl = `${http}://${host}/configurators/${user.organizationId}/${id}`
+    const previewUrl = `${http}://${host}/configurators/${user?.organizationId}/${id}`
 
     return (
       <PageContainer
